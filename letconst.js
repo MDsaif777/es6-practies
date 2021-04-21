45097143af4403cc5add30574757be831416e9c5
MD Saifur Rahman <72568300+MDsaif777@users.noreply.github.com>
2021-04-21 12:43:40 +0600

    put

---
+++ letconst.js
@@ -0,0 +1,19 @@
+const habby = 'Elias Kanson'
+// habby ="Omar Suny"
+console.log(habby);
+
+const numbers =[12, 14];
+numbers[1] = 88;
+numbers.push(12);
+// numbers =['kala', 'dola', 'Mala']
+console.log(numbers);
+
+let patientName = "Rahim Chaca";
+patientName = "Fatema Khala"
+console.log(patientName);
+
+let sum = 0;
+for(let i=0; i<10; i++){
+    sum = sum+i;
+}
+console.log(i)
